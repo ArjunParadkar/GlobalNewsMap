d8c39bf674c1fb774aa521f65e50cdeb20f900af
Henry Akins <henryakins08@gmail.com>
2025-09-26 07:45:48 -0500

    add a nice welcome

--- newsGrabber.py
+++ newsGrabber.py
@@ -1,4 +1,5 @@
 from newsapi import NewsApiClient
+print("Welcome to Global News Map 🌍")
 newsapi = NewsApiClient(api_key= "Enter API Key Here")
 top_headlines = newsapi.get_top_headlines(country='us')
 print(top_headlines)
